aa014f501ecf29a45b6b06c187097915abaa41fc
Knueven <bknueve@s1023820.srn.sandia.gov>
2019-04-04 13:20:19 -0600

    more UC formulations

--- egret/models/tests/test_unit_commitment.py
+++ egret/models/tests/test_unit_commitment.py
@@ -1,3 +1,12 @@
+#  ___________________________________________________________________________
+#
+#  EGRET: Electrical Grid Research and Engineering Tools
+#  Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+#  (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+#  Government retains certain rights in this software.
+#  This software is distributed under the Revised BSD License.
+#  ___________________________________________________________________________
+
 '''
 unit commitment tester
 '''
@@ -11,7 +20,6 @@ from egret.models.unit_commitment import *
 from egret.data.model_data import ModelData
 
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
 test_cases = [os.path.join(current_dir,'uc_test_instances', 'test_case_{}.json'.format(i)) for i in range(1,6)]
 test_objvals = [4218219.415648284, 5476393.707647476, 6023692.988920635, 5484256.671628478, 6091360.072517988]
 
@@ -23,6 +31,11 @@ def _test_uc_model(uc_model):
         md = ModelData(md_dict)
         model = uc_model(md)
         opt = SolverFactory('cbc')
+        opt.options['Rens'] = 'on'
+        opt.options['DivingG'] = 'on'
+        opt.options['DivingC'] = 'off'
+        opt.options['Rins'] = 'often'
+        opt.options['passF'] = '10'
         result = opt.solve(model, tee=True)
 
         assert result.solver.termination_condition == TerminationCondition.optimal
@@ -33,3 +46,18 @@ def test_tight_uc_model():
 
 def test_compact_uc_model():
     _test_uc_model(create_compact_unit_commitment_model)
+
+def test_KOW_uc_model():
+    _test_uc_model(create_KOW_unit_commitment_model)
+
+def test_ALS_uc_model():
+    _test_uc_model(create_ALS_unit_commitment_model)
+
+def test_MLR_uc_model():
+    _test_uc_model(create_MLR_unit_commitment_model)
+
+def test_random1_uc_model():
+    _test_uc_model(create_random1_unit_commitment_model)
+
+def test_random2_uc_model():
+    _test_uc_model(create_random2_unit_commitment_model)
--- egret/models/unit_commitment.py
+++ egret/models/unit_commitment.py
@@ -26,7 +26,7 @@ def _get_uc_model(model_data, formulation_list):
 
 def create_tight_unit_commitment_model(model_data, network_constraints='power_balance_constraints'):
     '''
-    Create a new unit commitment model based on the "tight" formulation from
+    Create a new unit commitment model based on the "Tight" formulation from
     B. Knueven, J. Ostrowski, and J.-P. Watson. "On Mixed Integer Programming
     Formulations for the Unit Commitment Problem" (2018). Available:
     http://www.optimization-online.org/DB_FILE/2018/11/6930.pdf
@@ -61,7 +61,7 @@ def create_tight_unit_commitment_model(model_data, network_constraints='power_ba
 
 def create_compact_unit_commitment_model(model_data, network_constraints='power_balance_constraints'):
     '''
-    Create a new unit commitment model based on the "compact" formulation from
+    Create a new unit commitment model based on the "Compact" formulation from
     B. Knueven, J. Ostrowski, and J.-P. Watson. "On Mixed Integer Programming
     Formulations for the Unit Commitment Problem" (2018). Available:
     http://www.optimization-online.org/DB_FILE/2018/11/6930.pdf
@@ -90,6 +90,181 @@ def create_compact_unit_commitment_model(model_data, network_constraints='power_
                          'HB_production_costs',
                          'rajan_takriti_UT_DT',
                          'MLR_startup_costs',
+                         network_constraints,
+                       ]
+    return _get_uc_model(model_data, formulation_list)
+
+def create_KOW_unit_commitment_model(model_data, network_constraints='power_balance_constraints'):
+    '''
+    Create a new unit commitment model based on the formulation from
+    B. Knueven, J. Ostrowski, and J.-P. Watson. "A Novel Matching 
+    Formulation for Startup Costs in Unit Commitment" (2018).
+    Available: http://www.optimization-online.org/DB_FILE/2017/03/5897.pdf
+
+    Parameters
+    ----------
+    model_data : egret.data.ModelData
+        An egret ModelData object with the appropriate data loaded.
+        # TODO: describe the required and optional attributes
+    network_constraints : str (optional)
+        Set of network constraints to use. The default option uses a B-\\theta
+        "DC" network.
+
+    Returns
+    -------
+        pyomo.environ.ConcreteModel unit commitment model
+
+    '''
+
+    formulation_list = [
+                         'garver_3bin_vars',
+                         'garver_power_vars', 
+                         'MLR_reserve_vars',
+                         'MLR_generation_limits',
+                         'MLR_ramping', 
+                         'KOW_production_costs',
+                         'rajan_takriti_UT_DT', 
+                         'KOW_startup_costs',
+                         network_constraints,
+                       ]
+    return _get_uc_model(model_data, formulation_list)
+
+def create_ALS_unit_commitment_model(model_data, network_constraints='power_balance_constraints'):
+    '''
+    Create a new unit commitment model based on the formulation from
+    Atakan, Semih, Guglielmo Lulli, and Suvrajeet Sen. "A state transition 
+    MIP formulation for the unit commitment problem." IEEE Transactions on 
+    Power Systems 33.1 (2018): 736-748.
+
+    Parameters
+    ----------
+    model_data : egret.data.ModelData
+        An egret ModelData object with the appropriate data loaded.
+        # TODO: describe the required and optional attributes
+    network_constraints : str (optional)
+        Set of network constraints to use. The default option uses a B-\\theta
+        "DC" network.
+
+    Returns
+    -------
+        pyomo.environ.ConcreteModel unit commitment model
+
+    '''
+
+    formulation_list = [
+                         'ALS_state_transition_vars',
+                         'garver_power_vars',
+                         'CA_power_avail_vars',
+                         'OAV_generation_limits',
+                         'ALS_damcikurt_ramping',
+                         'basic_production_costs_envelope',
+                         'rajan_takriti_UT_DT_2bin',
+                         'ALS_startup_costs',
+                         network_constraints,
+                       ]
+    return _get_uc_model(model_data, formulation_list)
+
+def create_MLR_unit_commitment_model(model_data, network_constraints='power_balance_constraints'):
+    '''
+    Create a new unit commitment model based on the formulation from
+    Morales-España, Germán, Jesus M. Latorre, and Andres Ramos. "Tight and 
+    compact MILP formulation for the thermal unit commitment problem." IEEE 
+    Transactions on Power Systems 28.4 (2013): 4897-4908.
+
+    Parameters
+    ----------
+    model_data : egret.data.ModelData
+        An egret ModelData object with the appropriate data loaded.
+        # TODO: describe the required and optional attributes
+    network_constraints : str (optional)
+        Set of network constraints to use. The default option uses a B-\\theta
+        "DC" network.
+
+    Returns
+    -------
+        pyomo.environ.ConcreteModel unit commitment model
+
+    '''
+
+    formulation_list = [
+                         'garver_3bin_vars',
+                         'garver_power_vars',
+                         'MLR_reserve_vars',
+                         'MLR_generation_limits',
+                         'MLR_ramping',
+                         'HB_production_costs',
+                         'rajan_takriti_UT_DT',
+                         'MLR_startup_costs',
+                         network_constraints,
+                       ]
+    return _get_uc_model(model_data, formulation_list)
+
+def create_random1_unit_commitment_model(model_data, network_constraints='power_balance_constraints'):
+    '''
+    Create a new unit commitment model based on the "Random1" formulation from
+    B. Knueven, J. Ostrowski, and J.-P. Watson. "On Mixed Integer Programming
+    Formulations for the Unit Commitment Problem" (2018). Available:
+    http://www.optimization-online.org/DB_FILE/2018/11/6930.pdf
+
+    Parameters
+    ----------
+    model_data : egret.data.ModelData
+        An egret ModelData object with the appropriate data loaded.
+        # TODO: describe the required and optional attributes
+    network_constraints : str (optional)
+        Set of network constraints to use. The default option uses a B-\\theta
+        "DC" network.
+
+    Returns
+    -------
+        pyomo.environ.ConcreteModel unit commitment model
+
+    '''
+
+    formulation_list = [
+                        'ALS_state_transition_vars',
+                        'garver_power_vars',
+                        'garver_power_avail_vars',
+                        'MLR_generation_limits',
+                        'ALS_damcikurt_ramping',
+                        'HB_production_costs',
+                        'rajan_takriti_UT_DT_2bin',
+                        'MLR_startup_costs2',
+                         network_constraints,
+                       ]
+    return _get_uc_model(model_data, formulation_list)
+
+def create_random2_unit_commitment_model(model_data, network_constraints='power_balance_constraints'):
+    '''
+    Create a new unit commitment model based on the "Random2" formulation from
+    B. Knueven, J. Ostrowski, and J.-P. Watson. "On Mixed Integer Programming
+    Formulations for the Unit Commitment Problem" (2018). Available:
+    http://www.optimization-online.org/DB_FILE/2018/11/6930.pdf
+
+    Parameters
+    ----------
+    model_data : egret.data.ModelData
+        An egret ModelData object with the appropriate data loaded.
+        # TODO: describe the required and optional attributes
+    network_constraints : str (optional)
+        Set of network constraints to use. The default option uses a B-\\theta
+        "DC" network.
+
+    Returns
+    -------
+        pyomo.environ.ConcreteModel unit commitment model
+
+    '''
+
+    formulation_list = [
+                        'garver_3bin_vars',
+                        'garver_power_vars',
+                        'garver_power_avail_vars',
+                        'pan_guan_gentile_KOW_generation_limits',
+                        'OAV_ramping_enhanced_2period',
+                        'HB_production_costs',
+                        'rajan_takriti_UT_DT',
+                        'MLR_startup_costs',
                          network_constraints,
                        ]
     return _get_uc_model(model_data, formulation_list)
