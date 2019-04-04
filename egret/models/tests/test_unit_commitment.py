73f5af0ad71ff7b57bdd47b7fe8c28376e50e2de
Knueven <bknueve@s1023820.srn.sandia.gov>
2019-04-04 09:24:32 -0600

    generalizing testing

--- egret/models/tests/test_unit_commitment.py
+++ egret/models/tests/test_unit_commitment.py
@@ -15,15 +15,18 @@ current_dir = os.path.dirname(os.path.abspath(__file__))
 test_cases = [os.path.join(current_dir,'uc_test_instances', 'test_case_{}.json'.format(i)) for i in range(1,6)]
 test_objvals = [4218219.415648285, 5476393.707647482, 6023692.988920629, 5484256.685824468, 6091360.072517976]
 
-def test_tight_uc_model():
+def _test_uc_model(uc_model):
 
     for test_case, ref_objval in zip(test_cases, test_objvals):
     
         md_dict = json.load(open(test_case,'r'))
         md = ModelData(md_dict)
-        model = create_tight_unit_commitment_model(md)
+        model = uc_model(md)
         opt = SolverFactory('glpk')
         result = opt.solve(model, tee=False)
 
         assert result.solver.termination_condition == TerminationCondition.optimal
         assert math.isclose(ref_objval, model.TotalCostObjective())
+
+def test_tight_uc_model():
+    _test_uc_model(create_tight_unit_commitment_model)
