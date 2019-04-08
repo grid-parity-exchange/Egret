6e2b8b5eccc8e05aa765695103ca20f3fed4ca4c
Laird <carldlaird@users.noreply.github.com>
2019-04-08 17:11:35 -0500

    fixing the log tests so they run from arbitrary directories

--- egret/common/tests/test_log.py
+++ egret/common/tests/test_log.py
@@ -12,26 +12,31 @@ This module tests the egret logger. Note that the pytest infrastructure captures
 therefore, we make an external call to bypass the pytest capturing
 """
 import subprocess
+import os
 
 msg = 'test message'
 msgcheck = b'test message\n'
 blankmsgcheck = b''
 
+test_dir = os.path.dirname(os.path.realpath(__file__))
+
+def run_logging_ext(level):
+    proc = subprocess.Popen(['python', 
+                             os.path.join(test_dir, 'logging_ext.py'),
+                             level, msg], 
+                            stdout=subprocess.PIPE, 
+                            stderr=subprocess.STDOUT)
+    return proc.communicate()
+
 def test_logging():
     # these should not show output by default
     for level in ['debug']:
-        proc = subprocess.Popen(['python', 'logging_ext.py', level, msg], 
-                                stdout=subprocess.PIPE, 
-                                stderr=subprocess.STDOUT)
-        out, err = proc.communicate()
+        out, err = run_logging_ext(level)
         assert out == blankmsgcheck
 
     # these should show output by defult
     for level in ['info', 'warning', 'error', 'critical']:
-        proc = subprocess.Popen(['python', 'logging_ext.py', level, msg], 
-                                stdout=subprocess.PIPE, 
-                                stderr=subprocess.STDOUT)
-        out, err = proc.communicate()
+        out, err = run_logging_ext(level)
         assert out == msgcheck
 
 
