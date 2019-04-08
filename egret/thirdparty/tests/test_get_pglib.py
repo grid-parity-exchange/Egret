9410f71394ae80d8852d608304ed374548337650
Laird <carldlaird@users.noreply.github.com>
2019-04-08 17:36:39 -0500

    adding copyright

--- egret/thirdparty/tests/test_get_pglib.py
+++ egret/thirdparty/tests/test_get_pglib.py
@@ -1,3 +1,12 @@
+#  ___________________________________________________________________________
+#
+#  EGRET: Electrical Grid Research and Engineering Tools
+#  Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+#  (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+#  Government retains certain rights in this software.
+#  This software is distributed under the Revised BSD License.
+#  ___________________________________________________________________________
+
 import pytest
 import os
 from pyomo.common.fileutils import this_file
