642931138f5cebe0e00d3dd2dc4cde04ba277467
Castillo <arcasti@s1032300.srn.sandia.gov>
2019-05-08 14:33:41 -0600

    updates for Ben's UC integration to power flows

--- egret/model_library/unit_commitment/power_balance.py
+++ egret/model_library/unit_commitment/power_balance.py
@@ -11,7 +11,136 @@
 from pyomo.environ import *
 import math
 
-from .uc_utils import add_model_attr 
+from .uc_utils import add_model_attr
+
+import pyomo.environ as pe
+import egret.model_library.transmission.tx_utils as tx_utils
+import egret.model_library.transmission.tx_calc as tx_calc
+import egret.model_library.transmission.bus as libbus
+import egret.model_library.transmission.branch as libbranch
+import egret.model_library.transmission.gen as libgen
+
+from egret.model_library.defn import CoordinateType, ApproximationType
+from math import pi
+
+
+def btheta_dcopf_network_model(md,block):
+    gens = dict(md.elements(element_type='generator'))
+    buses = dict(md.elements(element_type='bus'))
+    branches = dict(md.elements(element_type='branch'))
+    loads = dict(md.elements(element_type='load'))
+    shunts = dict(md.elements(element_type='shunt'))
+
+    gen_attrs = md.attributes(element_type='generator')
+    bus_attrs = md.attributes(element_type='bus')
+    branch_attrs = md.attributes(element_type='branch')
+
+    inlet_branches_by_bus, outlet_branches_by_bus = \
+        tx_utils.inlet_outlet_branches_by_bus(branches, buses)
+    gens_by_bus = tx_utils.gens_in_service_by_bus(buses, gens)
+
+    ### declare (and fix) the loads at the buses
+    bus_p_loads, _ = tx_utils.dict_of_bus_loads(buses, loads)
+
+    libbus.declare_var_pl(block, bus_attrs['names'], initialize=bus_p_loads)
+    block.pl.fix()
+
+    ### declare the fixed shunts at the buses
+    _, bus_gs_fixed_shunts = tx_utils.dict_of_bus_fixed_shunts(buses, shunts)
+
+    ### declare the polar voltages
+    va_bounds = {k: (-pi, pi) for k in bus_attrs['va']}
+    libbus.declare_var_va(block, bus_attrs['names'], initialize=bus_attrs['va'],
+                          bounds=va_bounds
+                          )
+
+    ### fix the reference bus
+    ref_bus = md.data['system']['reference_bus']
+    block.va[ref_bus].fix(0.0)
+
+    ref_angle = md.data['system']['reference_bus_angle']
+    if ref_angle != 0.0:
+        raise ValueError('The BTHETA DCOPF formulation currently only supports'
+                         ' a reference bus angle of 0 degrees, but an angle'
+                         ' of {} degrees was found.'.format(ref_angle))
+
+    ### declare the current flows in the branches
+    vr_init = {k: bus_attrs['vm'][k] * pe.cos(bus_attrs['va'][k]) for k in bus_attrs['vm']}
+    vj_init = {k: bus_attrs['vm'][k] * pe.sin(bus_attrs['va'][k]) for k in bus_attrs['vm']}
+    p_max = {k: branches[k]['rating_long_term'] for k in branches.keys()}
+    p_lbub = {k: (-p_max[k],p_max[k]) for k in branches.keys()}
+    pf_bounds = p_lbub
+    pf_init = dict()
+    for branch_name, branch in branches.items():
+        from_bus = branch['from_bus']
+        to_bus = branch['to_bus']
+        y_matrix = tx_calc.calculate_y_matrix_from_branch(branch)
+        ifr_init = tx_calc.calculate_ifr(vr_init[from_bus], vj_init[from_bus], vr_init[to_bus],
+                                         vj_init[to_bus], y_matrix)
+        ifj_init = tx_calc.calculate_ifj(vr_init[from_bus], vj_init[from_bus], vr_init[to_bus],
+                                         vj_init[to_bus], y_matrix)
+        pf_init[branch_name] = tx_calc.calculate_p(ifr_init, ifj_init, vr_init[from_bus], vj_init[from_bus])
+
+    libbranch.declare_var_pf(model=block,
+                             index_set=branch_attrs['names'],
+                             initialize=pf_init,
+                             bounds=pf_bounds
+                             )
+
+    ### declare the branch power flow approximation constraints
+    libbranch.declare_eq_branch_power_dc_approx(model=block,
+                                                index_set=branch_attrs['names'],
+                                                branches=branches,
+                                                approximation_type=ApproximationType.BTHETA
+                                                )
+
+    ### declare the p balance
+    libbus.declare_eq_p_balance_dc_approx(model=block,
+                                          index_set=bus_attrs['names'],
+                                          bus_p_loads=bus_p_loads,
+                                          gens_by_bus=gens_by_bus,
+                                          bus_gs_fixed_shunts=bus_gs_fixed_shunts,
+                                          inlet_branches_by_bus=inlet_branches_by_bus,
+                                          outlet_branches_by_bus=outlet_branches_by_bus,
+                                          approximation_type=ApproximationType.BTHETA
+                                          )
+
+    ### declare the real power flow limits
+    libbranch.declare_ineq_p_branch_thermal_lbub(model=block,
+                                                 index_set=branch_attrs['names'],
+                                                 branches=branches,
+                                                 p_thermal_limits=p_max,
+                                                 approximation_type=ApproximationType.BTHETA
+                                                 )
+
+    ### declare angle difference limits on interconnected buses
+    libbranch.declare_ineq_angle_diff_branch_lbub(model=block,
+                                                  index_set=branch_attrs['names'],
+                                                  branches=branches,
+                                                  coordinate_type=CoordinateType.POLAR
+                                                  )
+
+    ### declare the generator cost objective
+    libgen.declare_expression_pgqg_operating_cost(model=block,
+                                                  index_set=gen_attrs['names'],
+                                                  p_costs=gen_attrs['p_cost']
+                                                  )
+
+    return block
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 component_name = 'power_balance'
 
 #TODO: this doesn't check if storage_services is added first, 
