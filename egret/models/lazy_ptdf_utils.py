4d511ca704096e4215afae8b190305c501acc870
bknueven <bknueve@sandia.gov>
2019-07-18 08:40:17 -0600

    adding copyright statement

--- egret/models/lazy_ptdf_utils.py
+++ egret/models/lazy_ptdf_utils.py
@@ -1,3 +1,12 @@
+#  ___________________________________________________________________________
+#
+#  EGRET: Electrical Grid Research and Engineering Tools
+#  Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+#  (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+#  Government retains certain rights in this software.
+#  This software is distributed under the Revised BSD License.
+#  ___________________________________________________________________________
+
 ## helpers for flow verification across dcopf and unit commitment models
 from pyomo.solvers.plugins.solvers.persistent_solver import PersistentSolver
 import egret.model_library.transmission.branch as libbranch
