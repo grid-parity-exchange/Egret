3db6ae038439b87b71678b8860c111c2260c2a68
bknueven <bknueve@sandia.gov>
2019-08-08 18:35:42 -0600

    suppressing pyomo warning when adding item to set twice

--- egret/model_library/unit_commitment/params.py
+++ egret/model_library/unit_commitment/params.py
@@ -855,10 +855,9 @@ def load_params(model, model_data):
             thermal_gen_attrs['fuel_supply'] = dict()
         if 'aux_fuel_supply' not in thermal_gen_attrs:
             thermal_gen_attrs['aux_fuel_supply'] = dict()
-        for g in thermal_gen_attrs['fuel_supply'].keys():
-            yield g
-        for g in thermal_gen_attrs['aux_fuel_supply'].keys():
-            yield g
+        gen_set = set(thermal_gen_attrs['fuel_supply'].keys())
+        gen_set.update(thermal_gen_attrs['aux_fuel_supply'].keys())
+        return gen_set
 
     def gen_cost_fuel_validator(m,g):
         if 'p_fuel' in thermal_gen_attrs and g in thermal_gen_attrs['p_fuel']:
