240f6780181dc150a9625d1e2a85184858620158
bknueven <bknueve@sandia.gov>
2020-01-27 13:44:24 -0700

    adding copyright statement

--- examples/unit_commitment/test_uc_example.py
+++ examples/unit_commitment/test_uc_example.py
@@ -1,4 +1,11 @@
-
+#  ___________________________________________________________________________
+#
+#  EGRET: Electrical Grid Research and Engineering Tools
+#  Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+#  (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+#  Government retains certain rights in this software.
+#  This software is distributed under the Revised BSD License.
+#  ___________________________________________________________________________
 
 ## Example of solving a unit commitment problem in egret format
 ## from the test instance library
