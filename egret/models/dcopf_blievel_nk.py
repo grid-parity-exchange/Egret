14c42dc7026471013ba66e155897f27ed6f1be27
Castillo <arcasti@s1032300.srn.sandia.gov>
2020-02-17 21:52:44 -0700

    building model

---
+++ egret/models/dcopf_blievel_nk.py
@@ -0,0 +1,263 @@
+#  ___________________________________________________________________________
+#
+#  EGRET: Electrical Grid Research and Engineering Tools
+#  Copyright 2019 National Technology & Engineering Solutions of Sandia, LLC
+#  (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+#  Government retains certain rights in this software.
+#  This software is distributed under the Revised BSD License.
+#  ___________________________________________________________________________
+
+"""
+This module provides functions that create the modules for typical DCOPF formulations.
+
+#TODO: document this with examples
+"""
+import pyomo.environ as pe
+import pao.bilevel as bi
+import numpy as np
+import egret.model_library.transmission.tx_utils as tx_utils
+import egret.model_library.transmission.tx_calc as tx_calc
+import egret.model_library.transmission.bus as libbus
+import egret.model_library.transmission.branch as libbranch
+import egret.model_library.transmission.gen as libgen
+import egret.model_library.extensions.utils_bilevel_nk as utils
+import egret.model_library.extensions.master_bilevel_nk as cons
+import egret.model_library.decl as decl
+
+from egret.model_library.defn import CoordinateType, ApproximationType, BasePointType
+from egret.data.data_utils import map_items, zip_items
+from math import pi, radians
+
+def create_master(model_data, k=1):
+    md = model_data.clone_in_service()
+    tx_utils.scale_ModelData_to_pu(md, inplace = True)
+
+    relays = dict(md.elements(element_type='relay'))
+    gens = dict(md.elements(element_type='generator'))
+    buses = dict(md.elements(element_type='bus'))
+    loads = dict(md.elements(element_type='load'))
+    branches = dict(md.elements(element_type='branch'))
+
+    model = pe.ConcreteModel()
+
+    relay_branches = utils.dict_of_relay_branches(relays, branches)
+    branch_relays = utils.dict_of_branch_relays(relays, branches)
+    relay_branch_tuple = utils.relay_branch_tuple(relay_branches)
+    relay_gens = utils.dict_of_relay_gens(relays, gens)
+    gen_relays = utils.dict_of_gen_relays(relays, gens)
+    relay_gen_tuple = utils.relay_branch_tuple(relay_gens)
+    relay_loads = utils.dict_of_relay_loads(relays, loads)
+    load_relays = utils.dict_of_load_relays(relays, loads)
+    relay_load_tuple = utils.relay_branch_tuple(relay_loads)
+
+    decl.declare_var('z', model, None, initialize=0.0, domain=pe.NonNegativeReals)
+    decl.declare_var('delta', model, relays, domain=pe.Binary) # relays compromised
+    decl.declare_var('u', model, loads, domain=pe.Binary) # load available
+    decl.declare_var('v', model, branches, domain=pe.Binary) # generator available
+    decl.declare_var('w', model, gens, domain=pe.Binary) # line available
+
+    cons.declare_budget(model, k, relays)
+    cons.declare_load_compromised(model, relay_load_tuple)
+    cons.declare_load_uncompromised(model, loads, load_relays)
+    cons.declare_branch_compromised(model, relay_branch_tuple)
+    cons.declare_branch_uncompromised(model, branches, branch_relays)
+
+    model.obj = pe.Objective(expr=model.z, sense=pe.maximize)
+
+    return model, md
+
+
+def create_subproblem(model, model_data, include_angle_diff_limits=False):
+    md = model_data.clone_in_service()
+    tx_utils.scale_ModelData_to_pu(md, inplace = True)
+
+    gens = dict(md.elements(element_type='generator'))
+    buses = dict(md.elements(element_type='bus'))
+    branches = dict(md.elements(element_type='branch'))
+    loads = dict(md.elements(element_type='load'))
+    shunts = dict(md.elements(element_type='shunt'))
+
+    gen_attrs = md.attributes(element_type='generator')
+    bus_attrs = md.attributes(element_type='bus')
+    branch_attrs = md.attributes(element_type='branch')
+
+    inlet_branches_by_bus, outlet_branches_by_bus = \
+        tx_utils.inlet_outlet_branches_by_bus(branches, buses)
+    gens_by_bus = tx_utils.gens_by_bus(buses, gens)
+
+    model.subproblem = bi.SubModel(fixed=(model.delta, model.u, model.v, model.w))
+
+    ### declare (and fix) the loads at the buses
+    bus_p_loads, _ = tx_utils.dict_of_bus_loads(buses, loads)
+
+    libbus.declare_var_pl(model, bus_attrs['names'], initialize=bus_p_loads)
+    model.pl.fix()
+
+    ### declare the fixed shunts at the buses
+    _, bus_gs_fixed_shunts = tx_utils.dict_of_bus_fixed_shunts(buses, shunts)
+
+    ### declare the polar voltages
+    va_bounds = {k: (-pi, pi) for k in bus_attrs['va']}
+    libbus.declare_var_va(model, bus_attrs['names'], initialize=bus_attrs['va'],
+                          bounds=va_bounds
+                          )
+
+    ### fix the reference bus
+    ref_bus = md.data['system']['reference_bus']
+    ref_angle = md.data['system']['reference_bus_angle']
+    model.va[ref_bus].fix(radians(ref_angle))
+
+    ### declare the generator real power
+    pg_init = {k: (gen_attrs['p_min'][k] + gen_attrs['p_max'][k]) / 2.0 for k in gen_attrs['pg']}
+    libgen.declare_var_pg(model, gen_attrs['names'], initialize=pg_init,
+                          bounds=zip_items(gen_attrs['p_min'], gen_attrs['p_max'])
+                          )
+
+    ### declare the current flows in the branches
+    vr_init = {k: bus_attrs['vm'][k] * pe.cos(bus_attrs['va'][k]) for k in bus_attrs['vm']}
+    vj_init = {k: bus_attrs['vm'][k] * pe.sin(bus_attrs['va'][k]) for k in bus_attrs['vm']}
+    p_max = {k: branches[k]['rating_long_term'] for k in branches.keys()}
+    p_lbub = {k: (-p_max[k],p_max[k]) for k in branches.keys()}
+    pf_bounds = p_lbub
+    pf_init = dict()
+    for branch_name, branch in branches.items():
+        from_bus = branch['from_bus']
+        to_bus = branch['to_bus']
+        y_matrix = tx_calc.calculate_y_matrix_from_branch(branch)
+        ifr_init = tx_calc.calculate_ifr(vr_init[from_bus], vj_init[from_bus], vr_init[to_bus],
+                                         vj_init[to_bus], y_matrix)
+        ifj_init = tx_calc.calculate_ifj(vr_init[from_bus], vj_init[from_bus], vr_init[to_bus],
+                                         vj_init[to_bus], y_matrix)
+        pf_init[branch_name] = tx_calc.calculate_p(ifr_init, ifj_init, vr_init[from_bus], vj_init[from_bus])
+
+    libbranch.declare_var_pf(model=model,
+                             index_set=branch_attrs['names'],
+                             initialize=pf_init,
+                             bounds=pf_bounds
+                             )
+
+    ### declare the branch power flow approximation constraints
+    libbranch.declare_eq_branch_power_btheta_approx(model=model,
+                                                    index_set=branch_attrs['names'],
+                                                    branches=branches
+                                                    )
+
+    ### declare the p balance
+    libbus.declare_eq_p_balance_dc_approx(model=model,
+                                          index_set=bus_attrs['names'],
+                                          bus_p_loads=bus_p_loads,
+                                          gens_by_bus=gens_by_bus,
+                                          bus_gs_fixed_shunts=bus_gs_fixed_shunts,
+                                          inlet_branches_by_bus=inlet_branches_by_bus,
+                                          outlet_branches_by_bus=outlet_branches_by_bus,
+                                          approximation_type=ApproximationType.BTHETA
+                                          )
+
+    ### declare the real power flow limits
+    libbranch.declare_ineq_p_branch_thermal_lbub(model=model,
+                                                 index_set=branch_attrs['names'],
+                                                 branches=branches,
+                                                 p_thermal_limits=p_max,
+                                                 approximation_type=ApproximationType.BTHETA
+                                                 )
+
+    ### declare angle difference limits on interconnected buses
+    if include_angle_diff_limits:
+        libbranch.declare_ineq_angle_diff_branch_lbub(model=model,
+                                                      index_set=branch_attrs['names'],
+                                                      branches=branches,
+                                                      coordinate_type=CoordinateType.POLAR
+                                                      )
+
+    ### declare the generator cost objective
+    libgen.declare_expression_pgqg_operating_cost(model=model,
+                                                  index_set=gen_attrs['names'],
+                                                  p_costs=gen_attrs['p_cost']
+                                                  )
+
+    obj_expr = sum(model.pg_operating_cost[gen_name] for gen_name in model.pg_operating_cost)
+
+    model.obj = pe.Objective(expr=obj_expr)
+
+    return model, md
+
+def solve_bilevel_nk(model_data,
+                solver,
+                timelimit = None,
+                solver_tee = True,
+                symbolic_solver_labels = False,
+                options = None,
+                return_model = False,
+                return_results = False,
+                **kwargs):
+    '''
+    Create and solve a new worst-case attacker defender
+
+    Parameters
+    ----------
+    model_data : egret.data.ModelData
+        An egret ModelData object with the appropriate data loaded.
+    solver : str or pyomo.opt.base.solvers.OptSolver
+        Either a string specifying a pyomo solver name, or an instantiated pyomo solver
+    timelimit : float (optional)
+        Time limit for dcopf run. Default of None results in no time
+        limit being set.
+    solver_tee : bool (optional)
+        Display solver log. Default is True.
+    symbolic_solver_labels : bool (optional)
+        Use symbolic solver labels. Useful for debugging; default is False.
+    options : dict (optional)
+        Other options to pass into the solver. Default is dict().
+    dcopf_model_generator : function (optional)
+        Function for generating the dcopf model. Default is
+        egret.models.dcopf.create_btheta_dcopf_model
+    return_model : bool (optional)
+        If True, returns the pyomo model object
+    return_results : bool (optional)
+        If True, returns the pyomo results object
+    kwargs : dictionary (optional)
+        Additional arguments for building model
+    '''
+
+    import pyomo.environ as pe
+    import pyomo.opt as po
+    from pyomo.environ import value
+    from egret.common.solver_interface import _solve_model
+    from egret.model_library.transmission.tx_utils import \
+        scale_ModelData_to_pu, unscale_ModelData_to_pu
+
+    m, md = create_master(model_data)
+    m, md = create_subproblem(m, model_data)
+
+    m.dual = pe.Suffix(direction=pe.Suffix.IMPORT)
+
+    m, results, solver = _solve_model(m,solver,timelimit=timelimit,solver_tee=solver_tee,
+                              symbolic_solver_labels=symbolic_solver_labels,solver_options=options, return_solver=True)
+
+    # save results data to ModelData object
+    gens = dict(md.elements(element_type='generator'))
+    buses = dict(md.elements(element_type='bus'))
+    branches = dict(md.elements(element_type='branch'))
+
+    md.data['system']['total_cost'] = value(m.obj)
+
+    for g,g_dict in gens.items():
+        g_dict['pg'] = value(m.pg[g])
+
+    for k, k_dict in branches.items():
+        k_dict['pf'] = value(m.pf[k])
+
+    for b,b_dict in buses.items():
+        b_dict['pl'] = value(m.pl[b])
+        b_dict['lmp'] = value(m.dual[m.eq_p_balance[b]])
+        b_dict['va'] = value(m.va[b])
+
+    unscale_ModelData_to_pu(md, inplace=True)
+
+    if return_model and return_results:
+        return md, m, results
+    elif return_model:
+        return md, m
+    elif return_results:
+        return md, results
+    return md
