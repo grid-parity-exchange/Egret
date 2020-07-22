e7d5d80c551ac6ec0c272cafb9caa71d4e3c3a42
David L. Woodruff <dlwoodruff@ucdavis.edu>
2020-07-22 15:58:52 -0700

    expose slack flag

--- egret/models/ac_relaxations.py
+++ egret/models/ac_relaxations.py
@@ -31,19 +31,19 @@ def _relaxation_helper(model, md, include_soc, use_linear_relaxation):
                                    use_outer_approximation=use_linear_relaxation)
 
 
-def create_soc_relaxation(model_data, use_linear_relaxation=True):
-    model, md = _create_base_ac_model(model_data, include_feasibility_slack=False)
+def create_soc_relaxation(model_data, use_linear_relaxation=True, include_feasibility_slack=False):
+    model, md = _create_base_ac_model(model_data, include_feasibility_slack=include_feasibility_slack)
     _relaxation_helper(model=model, md=md, include_soc=True, use_linear_relaxation=use_linear_relaxation)
     return model, md
 
 
-def create_polar_acopf_relaxation(model_data, include_soc=True, use_linear_relaxation=True):
-    model, md = create_psv_acopf_model(model_data, include_feasibility_slack=False)
+def create_polar_acopf_relaxation(model_data, include_soc=True, use_linear_relaxation=True, include_feasibility_slack=False):
+    model, md = create_psv_acopf_model(model_data, include_feasibility_slack=include_feasibility_slack)
     _relaxation_helper(model=model, md=md, include_soc=include_soc, use_linear_relaxation=use_linear_relaxation)
     return model, md
 
 
-def create_rectangular_acopf_relaxation(model_data, include_soc=True, use_linear_relaxation=True):
-    model, md = create_rsv_acopf_model(model_data, include_feasibility_slack=False)
+def create_rectangular_acopf_relaxation(model_data, include_soc=True, use_linear_relaxation=True, include_feasibility_slack=False):
+    model, md = create_rsv_acopf_model(model_data, include_feasibility_slack=include_feasibility_slack)
     _relaxation_helper(model=model, md=md, include_soc=include_soc, use_linear_relaxation=use_linear_relaxation)
     return model, md
