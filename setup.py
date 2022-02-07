6b7c9c9040ddaf7b6d2b6850b50a088db6eaf0c7
Bernard Knueven <Bernard.Knueven@nrel.gov>
2022-02-07 16:26:46 -0700

    using python standard version names

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 from distutils.core import Extension
 
 DISTNAME = 'gridx-egret'
-VERSION = '0.5.2-dev'
+VERSION = '0.5.2.dev0'
 PACKAGES = find_packages()
 EXTENSIONS = []
 DESCRIPTION = 'EGRET: Electrical Grid Research and Engineering Tools.'
